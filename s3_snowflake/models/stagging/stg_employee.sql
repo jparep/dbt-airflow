b2dbf72149bc0b46b5f00d2001f75c36395f7c4a
jparep <parepson@gmail.com>
2025-02-17 22:42:19 -0700

    Stage employee

--- s3_snowflake/models/stagging/stg_employee.sql
+++ s3_snowflake/models/stagging/stg_employee.sql
@@ -1,7 +1,11 @@
--- Staging model for employee data
-SELECT
-    first AS first_name,
-    last AS last_name,
-    age,
-    sex AS gender
-FROM {{ source('raw', 'tbl') }};
+WITH employees_cleaned AS (
+    SELECT 
+        first AS first_name,       -- Standardized column name
+        last AS last_name,         -- Standardized column name
+        age,
+        UPPER(sex) AS gender       -- Convert gender to uppercase (Male/Female)
+    FROM {{ source('raw', 'tbl') }} -- Pulls from Snowflake
+    WHERE age IS NOT NULL          -- Remove any null ages
+)
+
+SELECT * FROM employees_cleaned;
